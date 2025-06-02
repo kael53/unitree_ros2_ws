1647700642bf49d5e63c79ed062bf80629685d27
Muhammed Kerem Kahraman <keremkahraman53@gmail.com>
2025-06-02 23:08:10 +0300

    Updated submodules and env setup.

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
 source /opt/ros/foxy/setup.bash
-source $HOME/unitree_ros2_ws/install/setup.bash
+source /unitree_ros2_ws/install/setup.bash
 export ROS_DOMAIN_ID=0
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI='file:///$HOME/unitree_ros2_ws/cyclonedds.xml'
+export CYCLONEDDS_URI='file:////unitree_ros2_ws/cyclonedds.xml'
